73a5f7c5149d99426a8f47870c06de8bf09501ef
tanpreet-coforge <tanpreet.coforge@gmail.com>
2026-04-09 12:10:14 +0530

    Enhance usePageView to update app state
    The custom hook now updates the app state with the current page information in addition to tracking page views.

--- src/hooks/usePageView.js
+++ src/hooks/usePageView.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { pushPageView } from '../services/dataLayer';
+import { updatePageState } from '../services/appState';
 
 /**
  * Custom hook for tracking page views 
+ * Automatically updates both dataLayer and appState
  * 
  * @param {string} pageName - Name of the page to track
  * @param {object} pageData - Additional page data (pageType, etc.)
@@ -16,8 +18,15 @@ export const usePageView = (pageName, pageData = {}) => {
   const location = useLocation();
 
   useEffect(() => {
-    // Track the page view
+    // Track the page view in dataLayer
     pushPageView(pageName, pageData);
+     
+    // Update app state with current page info
+    updatePageState({
+      name: pageName,
+      type: pageData.pageType,
+      url: location.pathname,
+    });
   }, [location.pathname]); // Only re-run when the actual route changes
 };
 
